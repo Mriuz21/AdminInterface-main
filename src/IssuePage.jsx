137a7c95535afb11cbf113df00a8ef0deaca8a64
MARIUS <marius.dudui03@e-uvt.ro>
2024-03-10 11:59:22 +0200

    commited12

--- src/IssuePage.jsx
+++ src/IssuePage.jsx
@@ -1,18 +1,22 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { ref, set } from 'firebase/database'; // Import ref and set from Firebase Realtime Database
 
 function IssuesPage({ data, database }) {
     const [status, setStatus] = useState({});
+    const [department, setDepartment] = useState({});
 
     useEffect(() => {
         if (data) {
             const newStatus = {};
+            const newDepartment = {};
             for (const id in data) {
                 if (data.hasOwnProperty(id)) {
                     newStatus[id] = data[id].status;
+                    newDepartment[id] = data[id].department;
                 }
             }
             setStatus(newStatus);
+            setDepartment(newDepartment);
         }
     }, [data]);
 
@@ -35,6 +39,24 @@ function IssuesPage({ data, database }) {
         }
     };
 
+    const handleChangeDepartment = async (id, event) => {
+        const newDepartment = event.target.value;
+        setDepartment(prevDepartment => ({
+            ...prevDepartment,
+            [id]: newDepartment
+        }));
+
+        try {
+            if (database && data && data[id]) {
+                await set(ref(database, `issues/${id}/department`), newDepartment);
+                console.log('Department updated successfully');
+            } else {
+                console.error('Database or issue not found');
+            }
+        } catch (error) {
+            console.error('Error updating department:', error);
+        }
+    };
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -51,6 +73,16 @@ function IssuesPage({ data, database }) {
                             <option value="Open">Open</option>
                             <option value="In-Progress">In-Progress</option>
                             <option value="Resolved">Resolved</option>
+                        </select></p>
+                        <p>Department: <select
+                            value={department[id] || issue.selectedOption}
+                            onChange={(event) => handleChangeDepartment(id, event)}
+                        >
+                            <option value="Unallocated">Unallocated</option>
+                            <option value="Drumuri si poduri">Drumuri si poduri</option>
+                            <option value="Mediu">Mediu</option>
+                            <option value="Ministerul afacerilor interne">MAI</option>
+                            {/* Add more departments as needed */}
                         </select></p>
                     </div>
                 </div>
